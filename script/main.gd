568cac00c0d4afd7d245bfe0f7f86a97f100e3e9
matt-ctrl-alt-del <vmatthewdaniel@gmail.com>
2025-10-19 21:56:41 -0700

    Script Changes_Minimal

--- script/main.gd
+++ script/main.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _on_killzone_body_entered(body: CharacterBody2D) -> void:
+func _on_killzone_body_entered(_body: CharacterBody2D) -> void:
 			audio_pain.play()
 			await get_tree().create_timer(0.2).timeout
 			get_tree().reload_current_scene()
