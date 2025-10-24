6bfe24f634f2423e9b1a86cc9c0e1227e0f2c565
matt-ctrl-alt-del <vmatthewdaniel@gmail.com>
2025-10-23 22:59:05 -0700

    Fixes

--- script/merchant.gd
+++ script/merchant.gd
@@ -26,7 +26,6 @@ func _on_interaction_area_body_exited(_body):
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 
-
 func _on_interact():
 	DialogManager.start_dialog(global_position, lines)
 	sprite.flip_h = true if interaction_area.get_overlapping_bodies()[0].global_position.x < global_position.x else false
